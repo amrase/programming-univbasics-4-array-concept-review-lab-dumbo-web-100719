512a76e6979fb225b1252050fcaf8e1434dff7cb
Amra Sezairi <amra.sezairi@hotmail.com>
2019-09-28 06:25:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,8 +13,8 @@ def find_max_value(array)
   # Add your solution here
   count =0 
   while count <array.length do 
-      if (array[count+1] > array[count])
-          return  array[count+1]
+      if (array[count]>x)
+          return  
       end
       count +=1 
   end
