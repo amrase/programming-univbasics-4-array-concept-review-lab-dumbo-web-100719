426268c12dff2bfa3d96c6812dfef447a96c7cdf
Amra Sezairi <amra.sezairi@hotmail.com>
2019-09-28 06:07:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   index
   while count < array.length do
       if (array[count]==value_to_find)
-         
+         index = count
       end
       count +=1 
   end
