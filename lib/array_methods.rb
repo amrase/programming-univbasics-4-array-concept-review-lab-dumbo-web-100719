18f2e486eb758aca96bcf7eac7ad897ecbeb6c52
Amra Sezairi <amra.sezairi@hotmail.com>
2019-09-28 06:20:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,7 @@ def find_max_value(array)
   count =0 
   while count <array.length do 
       if (array[count+1] > array[count])
-          return  array[count]
+          return  array[count+1]
       end
       count +=1 
   end
