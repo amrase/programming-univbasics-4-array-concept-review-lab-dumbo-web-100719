ea35a13cfd5856ad5d75f1c7bae846eab1f3839b
Amra Sezairi <amra.sezairi@hotmail.com>
2019-09-28 06:27:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,13 +12,7 @@ end
 def find_max_value(array)
   # Add your solution here
   count =0 
-  while count <array.length do 
-      if (array[count]>count)
-          count=array[count]
-          return count 
-      end
-      count +=1 
-  end
+  
 end
 
 def find_min_value(array)
